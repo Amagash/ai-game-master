59abd9dcb2b96a771b8a575a54e1eba32ee52874
Amagash <tiffanysouterre@gmail.com>
2025-02-12 17:29:34 +0100

    externalize prompt

---
+++ src/config/prompts.py
@@ -0,0 +1,4 @@
+LAUNCH_PROMPT = """
+The player's name is {player_name}. Welcome the player into the game. 
+Describe the surroundings of the player and create an atmosphere that the player can bounce off of.
+"""
