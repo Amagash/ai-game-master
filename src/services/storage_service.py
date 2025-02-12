29e30faf0aa7a5628b0a62536f6e8845bb720f1d
Amagash <tiffanysouterre@gmail.com>
2025-02-12 17:29:50 +0100

    fix pdf bug

--- src/services/storage_service.py
+++ src/services/storage_service.py
@@ -1,14 +1,22 @@
 import boto3
 from src.config.aws_config import S3_CONFIG
+from src.services.pdf_service import PDFService
 
 class StorageService:
     def __init__(self):
         self.s3_client = boto3.client('s3')
         self.bucket_name = S3_CONFIG['bucket_name']
     
-    def save_game_session(self, pdf_buffer, player_name):
-        filename = f"game_session_{player_name}.pdf"
+    def save_game_session(self, messages, player_name):
+        """Save game session as PDF to S3"""
         try:
+            # Convert messages to PDF
+            pdf_buffer = PDFService.create_chat_pdf(messages)
+            
+            # Generate filename
+            filename = f"game_session_{player_name}.pdf"
+            
+            # Upload to S3
             self.s3_client.upload_fileobj(
                 pdf_buffer,
                 self.bucket_name,
